5b81b83423998fbe71ab72523df7f82328b39f47
alextqy <289959263@qq.com>
2021-11-30 14:30:01 +0800

    update

--- solution/ServerBox/Program.cs
+++ solution/ServerBox/Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ServerBox
 {
@@ -20,7 +14,7 @@ namespace ServerBox
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls(ConfigHelper.AppSettingsHelper.GetSettings("URL")).UseStartup<Startup>();
                 });
     }
 }
