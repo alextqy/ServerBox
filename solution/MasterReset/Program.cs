2d64ac385fed5e29ca590704ee8bc64e38aa4760
alextqy <289959263@qq.com>
2021-11-30 10:06:59 +0800

    init

---
+++ solution/MasterReset/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MasterReset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
