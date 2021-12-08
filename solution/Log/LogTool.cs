faa262c8ffc9894fe7a0751769e5fb6d1cc2f8d3
alextqy <289959263@qq.com>
2021-12-08 17:45:38 +0800

    update

---
+++ solution/Log/LogTool.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Log
+{
+    public class LogTool
+    {
+        /// <summary>
+        /// 日志服务
+        /// </summary>
+        public static async void LogServer()
+        {
+            await Task.Delay(0);
+            while (true)
+            {
+                LogHandler();
+                Thread.Sleep(1500);
+            }
+        }
+
+        public static void LogHandler()
+        {
+
+        }
+
+    }
+}
