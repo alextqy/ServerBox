714943144e67fd7634d438529550e3fef081cb8d
alextqy <289959263@qq.com>
2021-12-08 10:44:29 +0800

    update

--- solution/ConfigHelper/AppSettingsHelper.cs
+++ solution/ConfigHelper/AppSettingsHelper.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
@@ -17,8 +16,14 @@ namespace ConfigHelper
 
         public static string GetSettings(string key)
         {
-            var _Configuration = new ConfigurationBuilder().AddJsonFile(Directory.GetParent("../") + "/" + "appsettings.json", optional: true, reloadOnChange: true).Build();
-            return _Configuration[key];
+            //var _Configuration = new ConfigurationBuilder().AddJsonFile(Directory.GetParent("../") + "/" + "appsettings.json", optional: true, reloadOnChange: true).Build();
+            //return _Configuration[key];
+
+            using StreamReader FilePath = new(Directory.GetParent("../") + "/" + "appsettings.json");
+            using JsonTextReader Reader = new(FilePath);
+            JObject JsonObject = (JObject)JToken.ReadFrom(Reader);
+            Reader.Close();
+            return (string)JsonObject[key];
         }
 
         public static bool WriteSettings(string k, string v)
