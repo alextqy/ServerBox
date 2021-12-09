2dacdcd4cbdd622e9ed8fa152db16feb89533182
alextqy <289959263@qq.com>
2021-12-09 16:37:47 +0800

    update

--- solution/SysLog/LogTool.cs
+++ solution/SysLog/LogTool.cs
@@ -7,6 +7,8 @@ namespace SysLog
 {
     public class LogTool
     {
+        public LogTool() { }
+
         /// <summary>
         /// 日志服务
         /// </summary>
@@ -22,9 +24,61 @@ namespace SysLog
 
         public static void LogHandler()
         {
-            var LogDir = Tools.RootPath() + "Log";
-            //Tools.CreateDir(LogDir);
-            Console.WriteLine(LogDir);
+            var LogDir = Tools.RootPath() + "Log/";
+            Tools.CreateDir(LogDir);
+            var LogFile = LogDir + DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString() + ".log";
+            Tools.CreateFile(LogFile);
         }
+
+        public static bool WriteLog(string Content)
+        {
+            var LogFile = Tools.RootPath() + "Log/" + DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString() + ".log";
+            Tools.CreateFile(LogFile);
+            return Tools.WriteFile(LogFile, Content, true);
+        }
+
+        public static string ReadLog(int TimeStamp = 0)
+        {
+            string SetLogDate;
+            if (TimeStamp == 0)
+            {
+                SetLogDate = DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString();
+            }
+            else
+            {
+                var Date = Tools.TimeStampToDateTime(TimeStamp);
+                SetLogDate = Date.Year.ToString() + Date.Month.ToString() + Date.Day.ToString();
+            }
+            var LogFile = Tools.RootPath() + "Log/" + SetLogDate + ".log";
+            if (!Tools.FileIsExists(LogFile))
+            {
+                return "";
+            }
+            else
+            {
+                return Tools.ReadFile(LogFile);
+            }
+        }
+
+        public static bool DelLog(int TimeStamp = 0)
+        {
+            string SetLogDate;
+            if (TimeStamp == 0)
+            {
+                SetLogDate = DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString();
+            }
+            else
+            {
+                var Date = Tools.TimeStampToDateTime(TimeStamp);
+                SetLogDate = Date.Year.ToString() + Date.Month.ToString() + Date.Day.ToString();
+            }
+            return Tools.DelFile(Tools.RootPath() + "Log/" + SetLogDate + ".log");
+        }
+
+        public static bool ClearLog()
+        {
+            return Tools.DelDir(Tools.RootPath() + "Log", true);
+        }
+
     }
 }
