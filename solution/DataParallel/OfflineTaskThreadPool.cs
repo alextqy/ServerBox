b783e16964e358eeddefde01026807d3e744dfa6
alextqy <289959263@qq.com>
2022-01-04 17:59:52 +0800

    coding

--- solution/DataParallel/OfflineTaskThreadPool.cs
+++ solution/DataParallel/OfflineTaskThreadPool.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace DataParallel
 {
-    //public class TaskProcessing
-    //{
-    //    public string Content { set; get; }
-    //    public TaskProcessing(string Content)
-    //    {
-    //        this.Content = Content;
-    //    }
-    //}
+    public class TaskProcessing
+    {
+        public string Content { set; get; }
+        public TaskProcessing(string Content)
+        {
+            this.Content = Content;
+        }
+    }
 
     public class OfflineTaskThreadPool
     {
         public static async void ProcessServer()
         {
+            //Tools.CorrectConsole("Process service is running!");
             await Task.Delay(0);
-            Tools.CorrectConsole("Process service is running!");
             Run();
         }
 
@@ -41,41 +41,29 @@ namespace DataParallel
             //});
             //Thread.Sleep(500);
             //Console.WriteLine(chatMsgs.Count);
+
+            var ProcessCount = Tools.EP();
+            Queue<TaskProcessing> OfflineTaskQueue = new();
+            while (true)
+            {
+                Thread.Sleep(1000);
+                if (ProcessCount > 0)
+                {
+                    if (OfflineTaskQueue.Count >= ProcessCount)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        // 入队列
+                        //new Task
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
         }
-
-        //public static void Run()
-        //{
-        //ProcessQueue<int> _processQueue = new();
-        //_processQueue.ProcessItemEvent += ProcessQueueProcessItemEvent;
-        //_processQueue.ProcessExceptionEvent += ProcessQueueProcessExceptionEvent;
-        //while (true)
-        //{
-        //Thread.Sleep(500);
-        //Console.WriteLine(_processQueue._isProcessing);
-        //_processQueue.Enqueue(3);
-        //_processQueue.Enqueue(2);
-        //_processQueue.Enqueue(1);
-        //Thread.Sleep(500);
-        //_processQueue.Dequeue(3);
-        //_processQueue.Dequeue(2);
-        //_processQueue.Dequeue(1);
-        //Thread.Sleep(500);
-        //Console.WriteLine(_processQueue._isProcessing);
-        //}
-        //}
-
-        /// <summary>
-        /// 该方法对入队的每个元素进行处理
-        /// </summary>
-        /// <param name="value"></param>
-        //private static void ProcessQueueProcessItemEvent(int value) { Console.WriteLine(value); }
-
-        /// <summary>
-        /// 处理异常
-        /// </summary>
-        /// <param name="obj">队列实例</param>
-        /// <param name="e">异常对象</param>
-        /// <param name="value">出错的数据</param>
-        //private static void ProcessQueueProcessExceptionEvent(dynamic obj, Exception e, int value) { Console.WriteLine(e.ToString()); }
     }
 }
